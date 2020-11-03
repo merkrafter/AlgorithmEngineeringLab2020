ccac30834f19ce81e6e02a629f568b7171d581b9
Mark Umnus <merkrafter@gmail.com>
2020-11-03 16:11:43 +0100

    Create a parallel version of the pi program (slide 22)
    This was accomplished using reduction() to keep the synchronization
    effort as low as possible.

--- answers/assignment01/pirallel_mc.cpp
+++ answers/assignment01/pirallel_mc.cpp
@@ -5,23 +5,28 @@
 using namespace std;
 
 int main() {
-  unsigned int seed = 0;
   int n = 100000000; // amount of points to generate
   int counter = 0;   // counter for points in the first quarter of a unit circle
   auto start_time =
       omp_get_wtime(); // omp_get_wtime() is an OpenMP library routine
 
-  default_random_engine re{seed};
-  uniform_real_distribution<double> zero_to_one{0.0, 1.0};
+#pragma omp parallel reduction(+ : counter)
+  {
+    random_device rd;
+    unsigned int seed = rd();
+    default_random_engine re{seed};
+    uniform_real_distribution<double> zero_to_one{0.0, 1.0};
 
-  // compute n points and test if they lie within the first quadrant of a unit
-  // circle
-  for (int i = 0; i < n; ++i) {
-    auto x = zero_to_one(re); // generate random number between 0.0 and 1.0
-    auto y = zero_to_one(re); // generate random number between 0.0 and 1.0
-    if (x * x + y * y <=
-        1.0) { // if the point lies in the first quadrant of a unit circle
-      ++counter;
+// compute n points and test if they lie within the first quadrant of a unit
+// circle
+#pragma omp for
+    for (int i = 0; i < n; ++i) {
+      auto x = zero_to_one(re); // generate random number between 0.0 and 1.0
+      auto y = zero_to_one(re); // generate random number between 0.0 and 1.0
+      if (x * x + y * y <=
+          1.0) { // if the point lies in the first quadrant of a unit circle
+        ++counter;
+      }
     }
   }
 
