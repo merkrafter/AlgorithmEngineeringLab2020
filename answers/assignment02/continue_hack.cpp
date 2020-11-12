4864200ac30fbc44b619e40ddc527b620c5ac5d9
Mark Umnus <merkrafter@gmail.com>
2020-11-12 14:26:11 +0100

    Implement function to find minimum in parallel fashion

--- answers/assignment02/continue_hack.cpp
+++ answers/assignment02/continue_hack.cpp
@@ -9,14 +9,13 @@ bool is_solution(int number) {  // test if number solves the problem
   return number > 133 && number < 140;
 }
 
-int main() {
+int get_solution_original() {
   constexpr int biggest_possible_number = 10000;
   // To avoid undefined code, it is necessary to use std::atomic for variables 
   // where race conditions may happen.
   // https://databasearchitects.blogspot.com/2020/10/c-concurrency-model-on-x86-for-dummies.html
   atomic<bool> solution_found(false);  // if true than we found the solution
   atomic<int> final_solution(INT32_MAX);
-  const double start = omp_get_wtime();
 
 #pragma omp parallel for schedule(dynamic)  // start parallel region
   for (int i = 0; i < biggest_possible_number; ++i) {
@@ -27,7 +26,34 @@ int main() {
       final_solution = i;
     }
   }  // end parallel region
-  // check if we've found a solution at all is omitted, you can add the check
-  cout << "The solution is: " << final_solution << endl;
+  return final_solution;
+}
+
+int get_solution_min() {
+  constexpr int biggest_possible_number = 10000;
+  atomic<int> final_solution(INT32_MAX);
+
+#pragma omp parallel for schedule(dynamic)  // start parallel region
+  for (int i = 0; i < biggest_possible_number; ++i) {
+    if(final_solution < i) {
+        continue;
+    }
+    if (is_solution(i)) {     // find some solution, not necessary the smallest
+      int previous = final_solution.load();
+      while(previous > i && !final_solution.compare_exchange_weak(previous,i)) {}
+    }
+  }  // end parallel region
+  return final_solution;
+}
+
+int main() {
+  double start = omp_get_wtime();
+  int final_solution = get_solution_original();
+  cout << "original:     The solution is: " << final_solution << endl;
+  cout << omp_get_wtime() - start << " seconds" << endl;
+
+  start = omp_get_wtime();
+  final_solution = get_solution_min();
+  cout << "min:          The solution is: " << final_solution << endl;
   cout << omp_get_wtime() - start << " seconds" << endl;
 }
