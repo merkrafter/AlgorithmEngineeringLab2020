540243cbd4f3d51ab876aed052cfc01789de2305
Mark Umnus <merkrafter@gmail.com>
2020-11-18 14:12:29 +0100

    Create own function for integrating the pi function

--- answers/assignment03/pirallel.cpp
+++ answers/assignment03/pirallel.cpp
@@ -4,16 +4,22 @@
 
 using namespace std;
 
+double integrate(const int from, const int to, const double width) {
+  double sum = 0.0;
+  for (int i = from; i < to; i++) {
+    const double x = (i + 0.5) * width;      // midpoint
+    sum += (1.0 / (1.0 + x * x)); // add new height of a rectangle
+  }
+  return sum;
+}
+
 int main() {
   int num_steps = 100000000;              // amount of rectangles
   double width = 1.0 / double(num_steps); // width of a rectangle
-  double sum = 0.0; // for summing up all heights of rectangles
-
   double start_time = omp_get_wtime(); // wall clock time in seconds
-  for (int i = 0; i < num_steps; i++) {
-    double x = (i + 0.5) * width;      // midpoint
-    sum = sum + (1.0 / (1.0 + x * x)); // add new height of a rectangle
-  }
+
+  double sum = integrate(0, num_steps, width); // for summing up all heights of rectangles
+
   double pi = sum * 4 * width; // compute pi
   double run_time = omp_get_wtime() - start_time;
 
