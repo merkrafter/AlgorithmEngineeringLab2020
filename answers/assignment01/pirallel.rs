629f05c020f715c408ac2cfd5208e06bd6074668
Mark Umnus <merkrafter@gmail.com>
2020-12-02 13:31:24 +0100

    Reimplement pirallel in Rust

---
+++ answers/assignment01/pirallel.rs
@@ -0,0 +1,24 @@
+use rayon::prelude::*;
+use std::time::Instant;
+
+fn main() {
+    const NUM_STEPS: u32 = 100_000_000;
+    const WIDTH: f64 = 1.0 / (NUM_STEPS as f64);
+    let mut sum = 0.0;
+
+    let start_time = Instant::now(); // monotonically increasing clock
+
+    for i in 0..NUM_STEPS {
+        let x = ((i as f64) + 0.5) * WIDTH;
+        sum += 1.0 / (1.0 + x * x);
+    }
+
+    let pi = sum * 4.0 * WIDTH;
+
+    let run_time = (start_time.elapsed().as_millis() as f64) / 1000.0;
+
+    println!(
+        "pi with {:} steps is {:} in {:} seconds",
+        NUM_STEPS, pi, run_time
+    );
+}
