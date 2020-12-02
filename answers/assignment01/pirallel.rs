283770f3f15e708021865c56e3a8b61889347972
Mark Umnus <merkrafter@gmail.com>
2020-12-02 13:36:51 +0100

    Refactor pirallel to be more "rusty"

--- answers/assignment01/pirallel.rs
+++ answers/assignment01/pirallel.rs
@@ -1,17 +1,15 @@
-use rayon::prelude::*;
 use std::time::Instant;
 
 fn main() {
     const NUM_STEPS: u32 = 100_000_000;
     const WIDTH: f64 = 1.0 / (NUM_STEPS as f64);
-    let mut sum = 0.0;
 
     let start_time = Instant::now(); // monotonically increasing clock
 
-    for i in 0..NUM_STEPS {
-        let x = ((i as f64) + 0.5) * WIDTH;
-        sum += 1.0 / (1.0 + x * x);
-    }
+    let sum: f64 = (0..NUM_STEPS)
+        .map(|i| ((i as f64) + 0.5) * WIDTH)
+        .map(|x| 1.0 / (1.0 + x * x))
+        .sum();
 
     let pi = sum * 4.0 * WIDTH;
 
